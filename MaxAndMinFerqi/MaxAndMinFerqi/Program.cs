f60048c706eaeb2998e575b9f08a1ca1db534f63
fuadaeff <fabdulla057@gmail.com>
2025-02-05 16:15:52 +0400

    Add files via upload

---
+++ MaxAndMinFerqi/MaxAndMinFerqi/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MaxAndMinFerqi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int[] numbers = [length];
+            for (int i = 0; i < length; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                numbers[i] = n;
+            }
+            int max = numbers[0];
+            int min = numbers[0];
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            if (min == 0)
+            {
+                Console.WriteLine("En kicik eded 0 oldugu ucun bolme emelliyyati yerine yetirmek mumkun deyil!");
+                return;
+            }
+            int multiple = max * min;
+            int division = max / min;
+            int result = multiple / division;
+            Console.WriteLine(result);
+            
+        }
+    }
+}
