337f047d871ebd65da44c632c630555c890e53b3
fuadaeff <fabdulla057@gmail.com>
2025-02-05 16:16:25 +0400

    Add files via upload

---
+++ Makulatura/Makulatura/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Makulatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qrup sayini daxil edin:");
+            int qrup_count = int.Parse(Console.ReadLine());
+            double[] numbers = [qrup_count];
+            for (int i = 0; i < qrup_count; i++)
+            {
+                double v = double.Parse(Console.ReadLine());
+                numbers[i] = v;
+            }
+            double max = numbers[0];
+            foreach (double num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine("{0:0.00}",max);
+        }
+    }
+}
